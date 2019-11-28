c5e1ca208ccf8938828c5b08391763950b38b064
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2019-11-28 17:39:02 +0300

    feat: add Room.findPath options
    DEV-471

--- build.js
+++ build.js
@@ -58,6 +58,39 @@ _extend(_docs, {
     }
 });
 
+_extend(_docs, {
+    roomFindPathOptions: _extend({}, _docs.pathfindingOptions, {
+        ignoreCreeps: {
+            "!doc": "Treat squares with creeps as walkable. Can be useful with too many moving creeps around or in some other cases. The default value is false.",
+            "!type": "boolean"
+        },
+        ignoreDestructibleStructures: {
+            "!doc": "Treat squares with destructible structures (constructed walls, ramparts, spawns, extensions) as walkable. The default value is false.",
+            "!type": "boolean"
+        },
+        ignoreRoads: {
+            "!doc": "Ignore road structures. Enabling this option can speed up the search. The default value is false. This is only used when the new PathFinder is enabled.",
+            "!type": "boolean"
+        },
+        ignore: {
+            "!doc": "An array of the room's objects or RoomPosition objects which should be treated as walkable tiles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).",
+            "!type": "[+RoomObject]"
+        },
+        avoid: {
+            "!doc": "An array of the room's objects or RoomPosition objects which should be treated as obstacles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).",
+            "!type": "[+RoomObject]"
+        },
+        serialize: {
+            "!doc": "If true, the result path will be serialized using Room.serializePath. The default is false.",
+            "!type": "boolean"
+        },
+        range: {
+            "!doc": "Find a path to a position in specified linear range of target. The default is 0.",
+            "!type": "number"
+        }
+    })
+});
+
 var def_screeps = {
     "!name": "screeps",
     "!define": {
@@ -87,6 +120,7 @@ var def_screeps = {
             "!type": "[PathStep]"
         },
         PathfindingOptions: _docs.pathfindingOptions,
+        RoomFindPathOptions: _docs.roomFindPathOptions,
         LookItem: {
             type: "string",
             creep: "+Creep",
@@ -1458,7 +1492,7 @@ _extend(def_screeps, {
             },
             findPath: {
                 "!doc": "Find an optimal path inside the room between fromPos and toPos using A* search algorithm.\n\nArguments:\n* fromPos - The start position.\n* toPos - The end position.\n* opts (optional) - An object containing additonal pathfinding flags:\n  - ignoreCreeps - Treat squares with creeps as walkable. Can be useful with too many moving creeps around or in some other cases. The default value is false.\n  - ignoreDestructibleStructures - Treat squares with destructible structures (constructed walls, ramparts, spawns, extensions) as walkable. Use this flag when you need to move through a territory blocked by hostile structures. If a creep with an ATTACK body part steps on such a square, it automatically attacks the structure. The default value is false.\n  - ignoreRoads - Ignore road structures. Enabling this option can speed up the search. The default value is false. This is only used when the new PathFinder is enabled.\n  - costCallback - You can use this callback to modify a CostMatrix for any room during the search. The callback accepts two arguments, roomName and costMatrix. Use the costMatrix instance to make changes to the positions costs. If you return a new matrix from this callback, it will be used instead of the built-in cached one. This option is only used when the new PathFinder is enabled.\n  - ignore - An array of the room's objects or RoomPosition objects which should be treated as walkable tiles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).\n  - avoid - An array of the room's objects or RoomPosition objects which should be treated as obstacles during the search. This option cannot be used when the new PathFinder is enabled (use costCallback option instead).\n  - maxOps - The maximum limit of possible pathfinding operations. You can limit CPU time used for the search based on ratio 1 op ~ 0.001 CPU. The default value is 2000.\n  - heuristicWeight - Weight to apply to the heuristic in the A* formula F = G + weight * H. Use this option only if you understand the underlying A* algorithm mechanics! The default value is 1.2.\n  - serialize - If true, the result path will be serialized using Room.serializePath. The default is false.\n  - maxRooms - The maximum allowed rooms to search. The default (and maximum) is 16. This is only used when the new PathFinder is enabled.\n  - range - Find a path to a position in specified linear range of target. The default is 0.\n  - plainCost - Cost for walking on plain positions. The default is 1.\n  - swampCost - Cost for walking on swamp positions. The default is 5.\n\nCPU cost: HIGH",
-                "!type": "fn(fromPos: +RoomPosition, toPos: +RoomPosition, opts?: object) -> Path"
+                "!type": "fn(fromPos: +RoomPosition, toPos: +RoomPosition, opts?: +RoomFindPathOptions) -> Path"
             },
             getPositionAt: {
                 "!doc": "Creates a RoomPosition object at the specified location.\n\nArguments:\n* x - The X position.\n* y - The Y Position.\n\nCPU cost: LOW",
